a06815f3cb94d56fe48042c16fd67befa395662c
Grégory <gregory2305@hotmail.fr>
2023-04-12 22:54:29 +0200

    model v5, queue, fix bibs.txt

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-import os
 import time
 import uuid
 import cv2
+import queue
 import threading
 import numpy as np
 import Levenshtein
@@ -12,15 +12,14 @@ from datetime import datetime
 from sort import Sort
 from ultralytics import YOLO
 
-
+QUEUE_MAX_SIZE = 100
 OCR_MIN_PROB = 0.7
-MODEL_MIN_PROB = 0.25
+MODEL_MIN_PROB = 0.10
 COLORS = [
     (255, 0, 0),    # blue
     (0, 255, 0),    # green
     (0, 0, 255),    # red
     (255, 0, 255),  # magenta
-    (0, 255, 255),  # yellow
     (255, 255, 0),  # cyan
 ]
 
@@ -28,18 +27,20 @@ class Server(object):
 
     image_queue = []
     track_dict = {}
-    running = False
 
     def __init__(self):
+        self.config = dotenv_values(".env")
+        self.bibs_list = open('bibs.txt', 'r').read().split('\n')
+
         # Init model
-        self.model = YOLO("yolov8n-v4.pt")
+        self.model = YOLO(self.config["MODEL"])
 
         # Init ocr
         self.reader = Reader(["fr"])
         self.bib_traker = Sort()
-        
-        self.config = dotenv_values(".env")
-        self.bibs_list = open('bibs.txt', 'r').read().split('\n')
+
+        # Init queue
+        self.queue = queue.Queue(QUEUE_MAX_SIZE)
 
     def cleanup_text(self, text):
         return "".join([c if ord(c) < 128 else "" for c in text]).strip()
@@ -92,12 +93,12 @@ class Server(object):
             bib = self.process_bib(track_id, cropped_image)
 
             # draw rectangle on the image
-            cv2.rectangle(img, (xA, yA), (xB, yB), COLORS[track_id%6], 3)
+            cv2.rectangle(img, (xA, yA), (xB, yB), COLORS[track_id%5], 3)
             
             # draw text on the image 
             text = f'id {track_id} : {bib}'
             text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 1, thickness=3)
-            cv2.rectangle(img, (xA, yA - text_size[1]), (xA + text_size[0], yA), COLORS[track_id%6], -1)
+            cv2.rectangle(img, (xA, yA - text_size[1]), (xA + text_size[0], yA), COLORS[track_id%5], -1)
             cv2.putText(img, text, (xA, yA), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 3, cv2.LINE_AA)
             
             
@@ -107,48 +108,41 @@ class Server(object):
         cv2.imshow('result', cv2.resize(img, (1152,648)))
         cv2.waitKey(1)
 
-    def worker(self): 
-        while self.running:
-            self.lock.acquire()
-            if len(self.image_queue) > 0:
-                item = self.image_queue.pop(0)
-                self.lock.release()
-                image = cv2.imread(item['path'])
-                os.remove(item['path'])
-                self.process_image(image)
-            else:
-                self.lock.release()
+    def process_images(self):
+        while True:
+            item = self.queue.get()
+            self.process_image(item['image'])
+            self.queue.task_done()
 
-    def run(self):
-        self.running = True
-
-        # Init worker thread
-        self.lock = threading.RLock()
-        thread = threading.Thread(target=self.worker)
-        # thread.start()
-        
+    def read_video(self):
         # Init camera
         cam = cv2.VideoCapture(self.config['SOURCE'])
-
+        
         if cam.isOpened() == False:
             print("Error")
 
-        ret = True
-        while ret:
+        while True:
             ret, img = cam.read()
 
-            self.process_image(img)
-            
-            # self.lock.acquire()
-            # item = {
-            #     'path': f'/tmp/{uuid.uuid4().hex}.png',
-            #     'date': datetime.now()
-            # } 
-            # cv2.imwrite(item['path'], img)
-            # self.image_queue.append(item)
-            # self.lock.release()
+            if not ret:
+                break
+
+            self.queue.put({
+                #'path': f'/tmp/{uuid.uuid4().hex}.png',
+                'image': img,
+                'date': datetime.now()
+            })
+
+    def start(self):
+        t1 = threading.Thread(target=self.read_video)
+        t2 = threading.Thread(target=self.process_images)
+
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
             
 
 if __name__ == "__main__":
     reader = Server()
-    reader.run()
+    reader.start()
